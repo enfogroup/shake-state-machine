9f013cd5f071548526cebf3f61091424a1b4ade7
Urban Pettersson <urbanpe88@gmail.com>
2021-06-08 20:14:29 +0200

    build: configure webpack

---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+const path = require('path')
+const TsConfigPathsPlugin = require('tsconfig-paths-webpack-plugin')
+const CopyPlugin = require('copy-webpack-plugin')
+const nodeExternals = require('webpack-node-externals')
+
+module.exports = {
+  context: __dirname,
+  mode: 'production',
+  entry: './src/index',
+  devtool: 'cheap-source-map',
+  resolve: {
+    extensions: ['.mjs', '.json', '.ts', 'js'],
+    symlinks: false,
+    plugins: [new TsConfigPathsPlugin()]
+  },
+  output: {
+    libraryTarget: 'commonjs',
+    path: path.join(__dirname, '.dist'),
+    filename: 'index.js'
+  },
+  externals: [nodeExternals()],
+  target: 'node',
+  module: {
+    rules: [
+      {
+        test: /\.(tsx?)$/,
+        loader: 'ts-loader',
+        include: path.resolve(__dirname, 'src'),
+        options: {
+          configFile: 'tsconfig.build.json'
+        }
+      }
+    ]
+  },
+  plugins: [
+    new CopyPlugin({
+      patterns: [
+        {
+          from: 'package.json',
+          to: '.'
+        },
+        {
+          from: 'README.MD',
+          to: '.'
+        }
+      ]
+    })
+  ]
+}
